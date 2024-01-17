17f97af3c2767922508d2750f2f59b317e28cf45
AlbertoMessineo <albevorox@gmail.com>
2024-01-17 10:53:00 +0100

    feat: inizio correzione es

--- index.php
+++ index.php
@@ -9,11 +9,13 @@ require_once __DIR__ .'/product.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>E-commerce per Cani & Gatti</title>
 </head>
 <body>
+    <h1>IT'S RAINING CATS AND DOGS</h1>
     <?php
-     $gioco1 = new product ('pallina' , 'gioco');
+     $gioco1 = new product ('pallina' , 'gioco' , 'cani', '10');
     ?>
     <p><?php var_dump($gioco1) ?></p>
 </body>
--- product.php
+++ product.php
@@ -2,13 +2,52 @@
 class product {
     public $name;
     public $type;
+    public $category;
+    public $price;
 
-    public function __construct($_name , $_type)
+    public function __construct($_name , $_type , $_category , $_price)
     {
-       $this->name =$_name;
-       $this->type =$_type;
+       $this->set_name($_name);
+       $this->set_type($_type);
+       $this->set_category($_category);
+       $this->set_price( $_price);
     }
+
+    //getters
+    public function get_name(){
+        return $this->name;
     }
+    public function get_type(){
+        return $this->type;
+    }
+    public function get_category(){
+        return $this->category;
+    }
+    public function get_price(){
+        return $this->price;
+    }
+    
+
+    //setters
+    public function set_name($_name){
+        $this->name = $_name ;
+    }
+    public function set_type($_type){
+        $this->type = $_type ;
+    }
+    public function set_category($_category){
+        $this->category = $_category ;
+    }
+    public function set_price($_price){
+        $this->price = $_price ;
+    }
+    
+
+
+
+
+    }
+
 class food extends product {
     public $name = 'croccantini';
 }
